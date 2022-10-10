a685ad565728fa8f2ee76360ed6718a3877f02fe
Eduard <ghiuzaneduard@gmail.com>
2022-10-10 13:32:12 +0300

    interface done

--- Hangman.java
+++ Hangman.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Hangman {
 
     public static String[] words = {"ant", "baboon", "badger", "bat", "bear", "beaver", "camel",
@@ -66,13 +68,53 @@ public class Hangman {
     "     |\n" +
     " =========\n"};
 
+    // ###############################################################################################
+    // ###############################################################################################
+    // ###############################################################################################
+
     public static void main(String[] args) {
 
+        while (true) {
+            clearScreen();
+            printGame();
+            inputLetter = scan.nextLine();
+            
+        }
 
+    }
 
+    // ###############################################################################################
+    // ###############################################################################################
+    // ###############################################################################################
 
+    static Scanner scan = new Scanner(System.in);
+    static String word = words[(int) (Math.random() * (words.length - 1))]; // generating a random word
+    static String[] wordArray = word.split("");
+    static int[] hiddenLetters = new int[words.length];
+    static String misses = "";
+    static String guess;
+    static String inputLetter;
 
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J"); 
+    }
 
+    public static void printGame() {
+        System.out.println(gallows[0]);
+        System.out.print("Word: ");
+        for (int i = 0; i < wordArray.length; i++) {
+            if (hiddenLetters[i] == 0) {
+                System.out.print("_ ");
+            }
+            else {
+                System.out.println(wordArray[i] + " ");
+            }
+        }
+        System.out.println();
+        System.out.print("Misses: " + misses);
+        System.out.println();
+        System.out.print("Guess: ");
+        
     }
 
 }
